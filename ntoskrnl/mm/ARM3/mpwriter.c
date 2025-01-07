9afd11c121321c0926236919cd308edfc74c0082
Thamatip Chitpong <thamatip.chitpong@reactos.org>
2025-01-07 15:56:13 +0700

    [NTOS:MM]

---
+++ ntoskrnl/mm/ARM3/mpwriter.c
@@ -0,0 +1,14 @@
+
+
+/* INCLUDES *******************************************************************/
+
+#include <ntoskrnl.h>
+#define NDEBUG
+#include <debug.h>
+
+#define MODULE_INVOLVED_IN_ARM3
+#include <mm/ARM3/miarm.h>
+
+/* FUNCTIONS *****************************************************************/
+
+
